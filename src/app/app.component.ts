4b2654a87687944bd0b819a9f7eb84b7ed7e7901
Murat Sari <wolfmanfx@gmail.com>
2023-08-31 16:30:44 +0200

    Update disable code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder, FormControl } from '@angular/forms';
+import { Component, inject } from "@angular/core";
+import { FormBuilder, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'ngModel';
+  title = "ngModel";
   disabled = false;
 
   fb = inject(FormBuilder).nonNullable.group({
-    title: ['formControlName'],
+    title: ["formControlName"],
   });
-  formControl = new FormControl('formControl', {nonNullable: true});
+  formControl = new FormControl("formControl", { nonNullable: true });
+
+  toggleDisable() {
+    this.disabled = !this.disabled;
+    if (this.disabled) {
+      this.formControl.disable();
+      this.fb.get("title")?.disable();
+    } else {
+      this.formControl.enable();
+      this.fb.get("title")?.enable();
+    }
+  }
 }
